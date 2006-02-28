2ef5df7a588617d19353b959a4d7ab076e262c9f
heiner <heiner@e17eae97-de05-0410-a4a2-dc618c93db62>
2006-02-28 11:03:03 +0000

    Preparing snapshot 3
    git-svn-id: svn+ssh://ftp.math.tu-dresden.de/home/st4/kuettler/.repositories/seal/trunk@58 e17eae97-de05-0410-a4a2-dc618c93db62

--- lib/about.rb
+++ lib/about.rb
@@ -7,7 +7,7 @@ class Seal
   DATE = SVN_DATE.match( /.*\((.*)\).*/ )[ 1 ]
   REV  = REVISION.match( /(\d+)/ )[ 1 ].to_i
 
-  SNAPSHOT = 2
+  SNAPSHOT = 3
   VERSION  = "0.1-pre (snapshot #{SNAPSHOT}/revision #{REV})"
 
   LICENSE = <<EOS
