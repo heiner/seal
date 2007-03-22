e3eabff4943d5816d7e1c71fb4135f4b3b07d4dc
kuettler <kuettler@e17eae97-de05-0410-a4a2-dc618c93db62>
2007-03-22 11:01:30 +0000

    Remove redundant file
    git-svn-id: svn+ssh://ftp.math.tu-dresden.de/home/st4/kuettler/.repositories/seal/seal/trunk@108 e17eae97-de05-0410-a4a2-dc618c93db62

--- src/build-snapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssh -T kuettler@www.math.tu-dresden.de <<EOS
-svn export file://$HOME/.repositories/seal/trunk $HOME/seal
-REVISION=`svnversion seal`
-echo REVISION=$REVISION";" DATE=\"`date -R`\" > $HOME/seal/src/snapshot.rb
-zip -r $HOME/public_html/seal/snapshots/seal-revision-$REVISION.zip $HOME/seal/
-rm -rf $HOME/seal/
-EOS
