7159e0b4dce64dbb5d641f815d155cd84540fd2c
kuettler <kuettler@e17eae97-de05-0410-a4a2-dc618c93db62>
2007-07-14 15:02:02 +0000

    Now with much more working
    git-svn-id: svn+ssh://ftp.math.tu-dresden.de/home/st4/kuettler/.repositories/seal/seal/branches/expat@115 e17eae97-de05-0410-a4a2-dc618c93db62

--- converter.rb
+++ converter.rb
@@ -2,132 +2,293 @@
 
 require 'xml/parser'
 
-class State
-  def startElement( converter, name, attr )
-  end
-  def endElement( converter, name )
-  end
-  def character( converter, data )
-  end
-end
+module States
 
-class RootState < State
-  def startElement( converter, name, attr )
-    if name == "body"
-      converter.newState( BodyState.new )
+  class State
+    def startElement( converter, name, attr )
+    end
+    def endElement( converter, name )
+    end
+    def character( converter, data )
     end
   end
-end
 
-class BodyState < State
-  def startElement( converter, name, attributes )
-    case name
-    when 'p'
-      converter.out << "\n\n"
-      converter.newState( TextState.new )
-    when 'pre'
-      case attributes[ 'class' ]
-      when /\btab\b/
-        converter.out << '\\begin{pre}%' << "\n"
+  PreTextState = State.new
+  class <<PreTextState
+
+    def chordline?( line )
+      line.rstrip!
+      rate = 0
+      rate += line.count(' ').quo(line.length)
+      rate += line.count('~').quo(line.length)
+      rate += 0.6 if line.length < 4
+      rate += 0.2 if line.include?( '#' )
+      rate += line.count('/')/4
+      rate += line.count('.')/10
+      rate += 0.1  if line.count('[') + line.count(']') >= 3
+      rate >= 0.6
+    end
+
+    def endElement( converter, name )
+      if name == "pre"
+        converter.buffer.slice!( 0 ) if converter.buffer[0] == ?\n
+        converter.buffer.chomp!
+        converter.buffer.gsub!( ' ', '~' )
+        converter.buffer.gsub!('--','{-}{-}')
+        converter.buffer.gsub!( '[', '{\\relax}[' )
+
+        converter.buffer.each_line do |line|
+          line.chomp!
+          if line.empty?
+            converter.out << "~\\\\\n"
+          elsif chordline?( line )
+            converter.out << line << "\\\\*\\relax\n"
+          else
+            converter.out << line << "\\\\ \\relax\n"
+          end
+        end
+
+        converter.out << "\\end{pre}"
         converter.buffer = ""
-        converter.newState( TabState.new )
-      when /\bverse\b/
-        converter.out << '\\begin{pre}%' << "\n"
-        converter.buffer = "verse"
-        converter.newState( PreState.new )
-      else
-        converter.newState( PreState.new )
+        converter.endState
       end
-    when 'h1'
-      converter.out << '\\section*{'
-      converter.newState( TextState.new )
+    end
+
+    def character( converter, data )
+      converter.buffer << data
     end
   end
 
-  def endElement( converter, name )
-    case name
-    when 'body'
-      converter.endState
+  TabState = State.new
+  class <<TabState
+    
+    def endElement( converter, name )
+      if name == "pre"
+        converter.buffer.rstrip!
+        converter.buffer.slice!( 0 ) if converter.buffer[0] == ?\n
+        converter.buffer.gsub!( '--', '{-}{-}' )
+        converter.buffer.gsub!( ' ', '~' )
+        converter.buffer.gsub!( "\t", '~'*8 )
+        converter.buffer.gsub!( "\n", "\\\\\\*\n" )
+        converter.out << converter.buffer << "\n" << '\\end{pre}'
+        converter.buffer = ""
+        converter.endState
+      end
+    end
+    
+    def character( converter, data )
+      converter.buffer << data
     end
   end
 
-  def character( converter, data )
-    converter.out << data
-  end
+  MiscPreState = State.new
+  class <<MiscPreState
+    def endElement( converter, name )
+      if name == "pre"
+        converter.out << '\\end{alltt}'
+        converter.endState
+      end
+    end
 
-end
-
-class TextState < State
-  def startElement( converter, name, attr )
-    case name
-    when 'i', 'em', 'a'
-      converter.out << '\\emph{'
-    when 'br'
-      converter.out << '\\\\'
+    def character( converter, data )
+      converter.out << data
     end
   end
 
-  def endElement( converter, name )
-    case name
-    when 'p'
-      #converter.out << "\n"
-      converter.endState
-    when 'h1'
-      converter.out << '}'
-      converter.endState
-    when 'i', 'em', 'a'
-      converter.out << '}'
+  SongTitleState = State.new
+  class <<SongTitleState
+    def endElement( converter, name )
+      if name == "h1"
+        converter.out << converter.buffer.gsub('!', "\\protect\\excl{}")
+        converter.out << '}{' << simplify( converter.buffer ) << "}"
+        converter.buffer = ""
+        converter.endState
+      else
+        raise Converter::Error
+      end
+    end
+
+    def character( converter, data )
+      converter.buffer << data
+    end
+
+    def simplify( title )
+      title.downcase.gsub( /\\?[#&~]|\\ss/, '' )
     end
   end
 
-  def character( converter, data )
-    converter.out << data
-  end
-
-  def skippedEntity( converter, name )
-    case name
-    when 'Oslash'
-      converter.out << '\\O'
-    end
-  end
-end
-
-class PreState < State
-  def endElement( converter, name )
-    if name == "pre"
-      converter.endState
+  RootState = State.new
+  class <<RootState
+    def startElement( converter, name, attr )
+      if name == "body"
+        converter.newState( BodyState )
+      end
     end
   end
 
-  def character( converter, data )
-    converter.out << ":" << data
-  end
-end
-
-class TabState < State
-
-  def endElement( converter, name )
-    if name == "pre"
-      converter.buffer.rstrip!
-      converter.buffer.slice!( 0 ) if converter.buffer[0] == ?\n
-      converter.buffer.gsub!( '--', '{-}{-}' )
-      converter.buffer.gsub!( ' ', '~' )
-      converter.buffer.gsub!( "\t", '~'*8 )
-      converter.buffer.gsub!( "\n", "\\\\\\*\n" )
-      converter.out << converter.buffer << "\n" << '\\end{pre}'
-      converter.buffer = ""
-      converter.endState
+  module PreHandler
+    def handle_pre( converter, attributes )
+      if not attributes[ 'class' ]
+        converter.out << '\\begin{alltt}'
+        converter.newState( MiscPreState )
+      else
+        classes = attributes[ 'class' ].split
+        if classes[1]
+          converter.out << "\\begin{#{classes[1]}}"
+          converter.extra = '\\end{' + classes[1] + '}' + converter.extra
+          classes.delete_at( 1 )
+        end
+        case classes[0]
+        when 'tab'
+          converter.out << '\\begin{pre}%' << "\n"
+          converter.buffer = ""
+          converter.newState( TabState )
+        when 'verse', 'refrain', 'bridge', 'bridge2', 'bridge3', 'spoken'
+          converter.out << '\\begin{' << classes[0] << '}\\begin{pre}%' << "\n"
+          converter.out << '\\slshape' if classes[0] == 'spoken'
+          converter.extra = '\\end{' + classes[0] + '}' + converter.extra
+          converter.newState( PreTextState )
+        when 'quote'
+          converter.out << '\\begin{quote}\\begin{alltt}'
+          converter.extra = '\\end{quote}' + converter.extra
+          converter.newState( MiscPreState )
+        when 'chords'
+          converter.out << '\\begin{alltt}'
+          converter.newState( MiscPreState )
+        else
+          raise Converter::Error, "unexpected pre attribute #{classes[0]}"          
+        end
+      end
     end
   end
   
-  def character( converter, data )
-    converter.buffer << data
+  BodyState = State.new
+  class <<BodyState
+
+    include PreHandler
+
+    def startElement( converter, name, attributes )
+      case name
+      when 'p'
+        converter.out << "\n\n"
+        converter.newState( TextState )
+      when 'pre'
+        handle_pre( converter, attributes )
+      when 'h1'
+        if attributes[ 'class' ] == 'songtitle'
+          converter.out << "\\songlbl{"
+          converter.newState( SongTitleState )
+        else
+          converter.out << '\\section*{'
+          converter.newState( TextState )
+        end
+      when 'h2'
+        if attributes[ 'class' ] == 'songversion'
+          converter.out << '\\songversion{'
+        else
+          converter.out << '\\subsection*{'
+        end
+        converter.newState( TextState )
+      when 'h3', 'h4'
+        converter.out << '\\subsubsection*{'
+        converter.newState( TextState )
+      when 'hr'
+        converter.out << "\n\\bobrule\n"
+      when 'ul'
+        converter.out << '\\begin{itemize}'
+      when 'li'
+        converter.out << '\\item '
+        converter.newState( TextState )
+      when 'div'
+      when 'blockquote'
+        converter.out << '\\begin{quote}'
+        converter.newState( TextState )
+      else
+        raise Converter::Error, "unexpected tag <#{name}>"
+      end
+    end
+
+    def endElement( converter, name )
+      case name
+      when 'ul'
+        converter.out << '\\end{itemize}'
+      when 'body'
+        converter.endState
+      end
+    end
+
+    def character( converter, data )
+      converter.out << data
+    end
   end
+
+  TextState = State.new
+  class <<TextState
+
+    include PreHandler
+
+    def startElement( converter, name, attr )
+      case name
+      when 'i', 'em', 'a'
+        converter.out << '\\emph{'
+      when 'b', 'strong'
+        converter.out << '\\textbf{'
+      when 'tt'
+        converter.out << '\\texttt{'
+      when 'br'
+        converter.out << '\\\\'
+      # when 'u', 'small', 'big', 'abbr'
+      when 'pre'
+        handle_pre( converter, attr )
+      when 'img'
+        image = attr[ 'src' ]
+        url = File.join( 'graphics', File.basename( image ) )
+        url = File.chopext( url )
+        converter.out << "\\input{#{url}}"
+      else
+        raise Converter::Error, "unexpected tag <#{name}>"
+      end
+    end
+
+    def endElement( converter, name )
+      case name
+      when 'p'
+        #converter.out << "\n"
+        converter.endState
+      when 'li'
+        converter.endState
+      when 'h1', 'h2', 'h3', 'h4'
+        converter.out << '}'
+        converter.endState
+      when 'i', 'em', 'a', 'b', 'strong', 'tt'
+        converter.out << '}'
+      when 'blockquote'
+        converter.out << '\\end{quote}'
+        converter.endState
+      end
+    end
+
+    def character( converter, data )
+      converter.out << data
+    end
+
+    def skippedEntity( converter, name )
+      case name
+      when 'Oslash'
+        converter.out << '\\O'
+      end
+    end
+  end
+
 end
+
 
 class Converter < XML::Parser
 
-  attr_accessor :out, :buffer
+  class Error < RuntimeError
+  end
+
+  attr_accessor :out, :buffer, :extra
 
   def initialize
     reset
@@ -136,8 +297,9 @@ class Converter < XML::Parser
   def reset( *args )
     super( *args )
     @out = nil
-    @states = [RootState.new]
+    @states = [States::RootState]
     @buffer = ""
+    @extra = ""
   end
 
   def startElement( name, attr )
@@ -162,7 +324,8 @@ class Converter < XML::Parser
   end
 
   def endState
-    @buffer = ""
+    self.out << extra
+    @extra = ""
     @states.pop
   end
 
@@ -178,13 +341,15 @@ class Converter < XML::Parser
   #end
 end
 
-converter = Converter.new
-converter.out = $stdout
-puts converter.setParamEntityParsing( 2 )
+if __FILE__ == $0
+  converter = Converter.new
+  converter.out = $stdout
+  #puts converter.setParamEntityParsing( 2 )
 
-begin
-  converter.convert( $<.read )
-rescue XML::ParserError
-  print "#{$0}: #{$!} (in line #{parser.line})\n"
-  exit 1
+  begin
+    converter.convert( $<.read )
+  rescue XML::Parser::Error, Converter::Error
+    print "#{$0}: #{$!} (in line #{converter.line})\n"
+    exit 1
+  end
 end
